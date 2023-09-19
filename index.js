2429411fb04fb14053beb9d95b2138e094902842
Shashmit kumar <shaashmit@gmail.com>
2023-09-19 17:30:07 +0530

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 require("dotenv").config();
 app.post("/", (req, res) => {
   try {
